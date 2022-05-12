a32d1f4e2f6d681ba2a12c45dc31c813991940d1
huy <huy@.com>
2022-05-12 15:43:38 +0700

    add comment

--- src/saga.js
+++ src/saga.js
@@ -5,7 +5,9 @@ import {getListPostSuccess} from './action';
 
 function* getListPostSaga(action) {
   try {
+    //call : Gọi function. Nếu nó return về một promise, tạm dừng saga cho đến khi promise được giải quyết
     const data  = yield call(getPostData);
+    //put : Dùng để dispatch một action
     yield put(getListPostSuccess(data));
   } catch (error) {
     //handle error
@@ -13,6 +15,8 @@ function* getListPostSaga(action) {
 }
 
 function* postsSaga() {
+  // takeLates : : Có nghĩa là nếu chúng ta thực hiện một loạt các actions,
+  // nó sẽ chỉ thực thi và trả lại kết quả của của actions cuối cùng
   yield takeLatest('GET_LIST_POST', getListPostSaga);
 }
 
